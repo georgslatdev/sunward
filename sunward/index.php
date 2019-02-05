ed2322b35b04072e8abf9c94476e08efda7b1cf5
Georgs Sorokins <georgs.sorokins@icloud.com>
2019-02-06 01:55:37 +0200

    Add files via upload

---
+++ sunward/index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SUNWARD | WELCOME</title>
+  <!-- Theme style -->
+	<link rel='stylesheet' href='css/theme.css'>
+	<link rel='stylesheet' href='css/normalize.css'>
+		<link rel='stylesheet' href='css/grid.css'>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <!-- Fontawesome icons -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+
+		<div class="header-language">
+				<div class="container">
+					<div class="drop-nav-language">
+						<button class="language-button">Valodas <i class="fa fa-caret-down"></i></button>
+							<div class="language-content" style="left:0">
+								<a href="#"><img src="images/us.png" alt="flag" style="width: 12px;"> Latviešu</a>
+								<a href="#"><img src="images/us.png" alt="flag" style="width: 12px;"> English</a>
+								<a href="#"><img src="images/us.png" alt="flag" style="width: 12px;"> Русский</a>
+							</div>
+					</div>
+				</div>
+			</div>
+
+				<nav class="main-nav">
+					<div class="container">
+							<a href="#"><img class="logo" src="images/logo.png" alt="sunward"></a>
+							<ul>
+								<li><a href="#">Sākums</a></li>
+								<li><a href="#">Par mums</a></li>
+								<li><a href="#">Produkti</a></li>
+								<li><a href="#">Serviss</a></li>
+								<li><a href="#">Finansēšana</a></li>
+								<li><a href="#">Jaunumi</a></li>
+								<li><a href="#">Kontakti</a></li>
+							</ul>
+						</div>
+				</nav>
+
+				<div class="sunward-slider">
+				<div class="container">
+					<div class="gallery">
+					  <figure class="text">
+					    <h1>Large Excavator</h1>
+							<p>Sunward large excavator is special excavators launched dedicated to mines, which boasts reliability, high efficiency, energy saving and comfort.</p>
+							<button class="big-button">Read more</button>
+					  </figure>
+							<ul class="carousel-indicators">
+							  <li data-target="#myCarousel" data-slide-to="0"></li>
+							  <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+							  <li data-target="#myCarousel" data-slide-to="2"></li>
+								<li data-target="#myCarousel" data-slide-to="3"></li>
+							</ul>
+
+					  <div class="controls">
+					    <a href="#item-1" class="control-button"></a>
+					    <a href="#item-2" class="control-button"></a>
+					    <a href="#item-3" class="control-button"></a>
+					  </div>
+					</div>
+				</div>
+			</div>
+	</header>
+
+		<div class="sunward-products">
+			<div class="container">
+				<h1>Sunward Europas produkti</h1>
+				<div class="row">
+				<div class="sunward-product-list">
+					 <img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator Megatron</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					 <img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				<div class="sunward-product-list">
+					<img src="images/1.png" alt="product" width="155" height="155">
+					 <h2>Excavator</h2>
+					 <button class="small-button-green">Read more</button>
+				</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="sunward-about">
+			<div class="container">
+					<h1>Par Sunward</h1>
+					<div class="row">
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body. Headquartered in Changsha City, Sunward has also set up several subsidiaries at home and abroad, with its footprints spread at Changsha, Wuxi, Huaibei and Europe. </p>
+						<p>Now, it has gradually grown into an international enterprise that focuses on engineering machinery, owns independent intellectual property and makes global influences, ranking among top 50 global enterprises manufacturing construction machineries.  </p>
+						<div class="slideshow">
+							<div><img class="big-img" src="images/1.png" alt="1"></div>
+							<div><img class="big-img" src="images/1.png" alt="2"></div>
+						</div>
+						<button class="big-button">Products</button>
+					</div>
+			</div>
+		</div>
+
+		<div class="sunward-news">
+			<div class="container">
+				<h1>Jaunumi</h1>
+				<div class="row">
+				<div class="news-column">
+					<div class="news-title">
+						<h1>Sunward Intelligent Equipment Group was founded in 1999</h1>
+					</div>
+					<div class="news-image">
+						<img src="images/1.png" alt="news" width="258" height="192" style="float:left; padding: 25px;">
+					</div>
+						<p class="date">Published: Thursday, 12 May 2016 13:47</p>
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body.</p>
+					<button class="small-button-green" style="word-wrap: break-word;float:right;">Lasīt vairāk <i class="fas fa-angle-right"></i></button>
+				</div>
+				<div class="news-column">
+					<div class="news-title">
+						<h1>Sunward Intelligent Equipment Group was founded in 1999</h1>
+					</div>
+					<div class="news-image">
+						<img src="images/1.png" alt="news" width="258" height="192" style="float:left; padding: 25px;">
+					</div>
+						<p class="date">Published: Thursday, 12 May 2016 13:47</p>
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body.</p>
+					<button class="small-button-green" style="word-wrap: break-word;float:right;">Lasīt vairāk <i class="fas fa-angle-right"></i></button>
+				</div>
+				<div class="news-column">
+					<div class="news-title">
+						<h1>Sunward Intelligent Equipment Group was founded in 1999</h1>
+					</div>
+					<div class="news-image">
+						<img src="images/1.png" alt="news" width="258" height="192" style="float:left; padding: 25px;">
+					</div>
+						<p class="date">Published: Thursday, 12 May 2016 13:47</p>
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body.</p>
+					<button class="small-button-green" style="word-wrap: break-word;float:right;">Lasīt vairāk <i class="fas fa-angle-right"></i></button>
+				</div>
+				<div class="news-column">
+					<div class="news-title">
+						<h1>Sunward Intelligent Equipment Group was founded in 1999</h1>
+					</div>
+					<div class="news-image">
+						<img src="images/1.png" alt="news" width="258" height="192" style="float:left; padding: 25px;">
+					</div>
+						<p class="date">Published: Thursday, 12 May 2016 13:47</p>
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body.</p>
+					<button class="small-button-green" style="word-wrap: break-word;float:right;">Lasīt vairāk <i class="fas fa-angle-right"></i></button>
+				</div>
+				<div class="news-column">
+					<div class="news-title">
+						<h1>Sunward Intelligent Equipment Group was founded in 1999</h1>
+					</div>
+					<div class="news-image">
+						<img src="images/1.png" alt="news" width="258" height="192" style="float:left; padding: 25px;">
+					</div>
+						<p class="date">Published: Thursday, 12 May 2016 13:47</p>
+						<p>Sunward Intelligent Equipment Group was founded in 1999 and Sunward Intelligent Equipment Co., Ltd. (stock short name: Sunward Intelligent; stock code: 002097) is its main body.</p>
+					<button class="small-button-green" style="word-wrap: break-word;float:right;">Lasīt vairāk <i class="fas fa-angle-right"></i></button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="bottom-info" style="width: 360px;">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamquis. </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamquis. </p>
+				</div>
+			<div class="bottom-info">
+
+			</div>
+			<div class="bottom-info">
+				<ul>
+	 				<li><a href='#'>Par mums</a></li>
+	 				<li><a href='#'>Produkti</a></li>
+	 				<li><a href='#'>Serviss</a></li>
+	 				<li><a href='#'>Finansēšana</a></li>
+					<li><a href='#'>Jaunumi</a></li>
+					<li><a href='#'>Kontakti</a></li>
+				</ul>
+			</div>
+			<div class="bottom-info">
+				<p>Contact</p>
+				<p>info@sunward.com</p>
+				<p>32-011-434666</p>
+				<p>Brīvības gatve 128</p>
+			</div>
+		</div>
+			<div class="red">
+				<div class="container">
+					<h1>©2019 - Sunward  |   All right reserved</h1>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
